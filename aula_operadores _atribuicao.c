50df23e739bc984c1c27b6e079944a28f607c60d
m-ssiqueira <mssiqueira@gmail.com>
2025-06-13 12:45:21 +0000

    exercicio simples de operadores de atribuição

---
+++ aula_operadores _atribuicao.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/*
+nessa operação, o operador faz o calculo e assume, atribui o valor à variavel
+*/
+    int main(){
+
+        int numero01 = 10, numero02 = 5;
+        int resultado;
+        printf("%d\n", resultado += numero01);
+        printf("%d\n", resultado -= numero02);
+        printf("%d\n", resultado *= 10);
+        printf("%d\n", resultado /= 2);
+
+        return 0;
+    }
